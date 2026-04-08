be824699f7ed9ad748b6cae1ce8971f2f66f6814
alejosanmi10-10 <alejosan2907@gmail.com>
2026-04-08 14:17:20 -0500

    Dev: Agregar proxy para /uploads en vite para resolver crash local de Vite tratando de analizar binarios como JS

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,8 @@ export default defineConfig({
     },
     server: {
         proxy: {
-            '/api': 'http://localhost:3001'
+            '/api': 'http://localhost:3001',
+            '/uploads': 'http://localhost:3001'
         }
     }
 })
